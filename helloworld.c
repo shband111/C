d2ae2d18ee0d3c5d4645854f30c8912963927dd2
shband111 <66156014+shband111@users.noreply.github.com>
2020-05-30 11:36:52 +0100

    Create helloworld.c
    This is the classic example which every coder loves when starting to learn a programming language, the "Hello World" program. How to start coding with C is represents by this example code for the very beginners.

---
+++ helloworld.c
@@ -0,0 +1,13 @@
+//include library files needed to run the program
+#include <stdio.h>
+
+//start the main part of the program
+int main()
+{
+    //print "Hello to the world of C" on the screen using the 'printf()' function
+    printf("Hello to the world of C");
+
+    //finally return value 0, and exit
+    return (0);
+
+} //end of the 'main'
